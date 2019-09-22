4b4a06aab3b75bc5d0260be38ba0e951267c0a33
ritesh4009 <ritesh4009@gmail.com>
2019-09-22 22:25:14 +0200

    working code

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,99 @@
 #include <Arduino.h>
 #include <Keypad.h>
+#include <Wire.h>
+#include <string.h>
 
-const byte ROWS = 5;
+const byte ROWS = 4;
 const byte COLUMNS =4;
-byte rowPins[ROWS] = {13,12,11,10,9}; //connect to row pinout of the keypad
-byte columnPins[COLUMNS] = {A2,A3,A0,A1}; //connect to column pinout of keypad
+byte rowPins[ROWS] = {5,4,3,2}; //connect to row pinout of the keypad
+byte columnPins[COLUMNS] = {6,7,8,9}; //connect to column pinout of keypad
 //Define symbols on keypad
 char hexakeys [ROWS][COLUMNS] = {
   {'A','B','#','*'},
   {'1','2','3','U'},
   {'4','5','6','D'},
-  {'7','8','9','E'},
-  {'<','0','>','C'}
+  {'7','8','9','E'}
 };
 
 Keypad Customkeypad = Keypad(makeKeymap (hexakeys),rowPins,columnPins,ROWS, COLUMNS);
 
+void receiveevent(int howmany);
+void requestEvent();
+int key;
+int d;
+
 void setup() {
   // put your setup code here, to run once:
+  Wire.begin(50);
+  Wire.onReceive (receiveevent);
+  Wire.onRequest (requestEvent);
+  pinMode(10,OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13,OUTPUT);  // LED
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  d =Customkeypad.getKey();
+
+  if (d!= 0)
+  {
+    Serial.println(d);
+    digitalWrite (13, HIGH);
+  }
+  
+  while (digitalRead(13))
+  {
+    /* do nothing */
+  }
+  
+}
+
+void receiveevent(int howmany)
+{
+  while (Wire.available())
+  {
+    char c = Wire.read();
+    switch (c)
+    {
+    case 'A':
+      digitalWrite(10, HIGH);
+      Serial.println(c);
+      break;
+    case 'a':
+      digitalWrite(10, LOW);
+      Serial.println(c);
+      break;
+    case 'B':
+      digitalWrite(11, HIGH);
+      Serial.println(c);
+      break;
+    case 'b':
+      digitalWrite(11, LOW);
+      Serial.println(c);
+      break;
+    case 'C':
+      digitalWrite(12, HIGH);
+      Serial.println(c);
+      break;
+    case 'c':
+      digitalWrite(12, LOW);
+      Serial.println(c);
+      break;
+    case 'K':
+      key = 1;
+      Serial.println(key);
+      break;
+    default:
+      break;
+    }   
+  }
+}
+
+void requestEvent() {
+  // Send response back to Master
+  Wire.write(d);
+  digitalWrite (13, LOW);
 }
